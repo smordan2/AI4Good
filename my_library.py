f36cb3fc10149f8c668c9d0e3e4c75c76d2ee39f
smordan2 <smordan@uoregon.edu>
2024-11-13 18:52:17 -0800

    my_library.py

--- my_library.py
+++ my_library.py
@@ -1,2 +1,39 @@
 def test_load():
   return 'loaded'
+def compute_probs(neg,pos):
+  p0 = neg/(neg+pos)
+  p1 = pos/(neg+pos)
+  return [p0,p1]
+def cond_prob(full_table, the_evidence_column, the_evidence_column_value, the_target_column, the_target_column_value):
+  assert the_evidence_column in full_table
+  assert the_target_column in full_table
+  assert the_evidence_column_value in up_get_column(full_table, the_evidence_column)
+  assert the_target_column_value in up_get_column(full_table, the_target_column)
+  t_subset = up_table_subset(full_table, the_target_column, 'equals', the_target_column_value)
+  e_list = up_get_column(t_subset, the_evidence_column)
+  p_b_a = sum([1 if v==the_evidence_column_value else 0 for v in e_list])/len(e_list)
+  return p_b_a
+def cond_probs_product(full_table, evidence_row, target_column, target_column_value):
+  assert target_column in full_table
+  assert target_column_value in up_get_column(full_table, target_column)
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1 
+  evidence_columns = up_list_column_names(full_table)[:-1]
+  evidence_complete = up_zip_lists(evidence_columns, evidence_row)
+  cond_prob_list = [cond_prob(full_table, evidence_column, evidence_value, target_column, target_column_value) for evidence_column, evidence_value in evidence_complete]
+  partial_numerator = up_product(cond_prob_list)
+  return partial_numerator
+def prior_prob(full_table, the_column, the_column_value):
+  assert the_column in full_table
+  assert the_column_value in up_get_column(full_table, the_column)
+  t_list = up_get_column(full_table, the_column)
+  p_a = sum([1 if v==the_column_value else 0 for v in t_list])/len(t_list)
+  return p_a
+def naive_bayes(full_table, evidence_row, target_column):
+  assert target_column in full_table
+  assert isinstance(evidence_row, list)
+  assert len(evidence_row) == len(up_list_column_names(full_table)) - 1 
+  neg = cond_probs_product(full_table, evidence_row, target_column, 0) * prior_prob(full_table, target_column, 0)
+  pos = cond_probs_product(full_table, evidence_row, target_column, 1) * prior_prob(full_table, target_column, 1)
+  neg, pos = compute_probs(neg, pos)
+  return [neg, pos] 
